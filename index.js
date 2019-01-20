4cb7506937dfd6733e81109ef4027ca29eb4ab4b
Max Clayton Clowes <maxcc@me.com>
2019-01-20 23:04:59 +0000

    stage 0 config

--- index.js
+++ index.js
@@ -1,44 +1,18 @@
 module.exports = {
 	presets: [
-		require.resolve("babel-preset-env"),
-		require.resolve("babel-preset-react"),
+		require.resolve("@babel/preset-env"),
+		require.resolve("@babel/preset-react"),
+		require.resolve('@babel/preset-stage-0'),
 	],
 	plugins: [
-		// plain
-		require.resolve("babel-plugin-add-react-displayname"),
-		require.resolve("babel-plugin-inline-react-svg"),
-		require.resolve("babel-plugin-preval"),
-		require.resolve("babel-plugin-syntax-dynamic-import"),
-		require.resolve("babel-plugin-transform-class-properties"),
-		require.resolve("babel-plugin-transform-decorators-legacy"),
-		require.resolve("babel-plugin-transform-export-extensions"),
-		require.resolve("babel-plugin-transform-regenerator"),
-
-		// configured
-		[	
-			require.resolve("babel-plugin-styled-components"),	
-			{ "ssr": true },	
-		],
+		require.resolve("@babel/plugin-syntax-dynamic-import"),
+		require.resolve("@babel/plugin-transform-regenerator"),
 
 		[
-			require.resolve("babel-plugin-transform-object-rest-spread"),
-			{ "useBuiltIns": true }
-		],
-		[
-			require.resolve("babel-plugin-styled-components"), 
-			{
-				"displayName": true,
-			},
-		],
-		[
-			require.resolve("babel-plugin-transform-runtime"),
+			require.resolve("@babel/plugin-transform-runtime"),
 			{
 				polyfill: false,
 			},
 		],	
-
-		// optional
-		require.resolve("@babel/plugin-proposal-optional-chaining"),
-		require.resolve("@babel/plugin-proposal-pipeline-operator"),
 	],
 };
