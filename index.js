825335d5e0250549ad8146f4ff04c4450063b27a
mcclowes <maxcc@me.com>
2019-03-18 15:52:40 +0000

    upgrades

--- index.js
+++ index.js
@@ -24,8 +24,10 @@ module.exports = {
 		"@babel/plugin-proposal-json-strings",
 		"@babel/plugin-syntax-dynamic-import",
 		"@babel/plugin-syntax-import-meta",
-		"@babel/plugin-proposal-optional-chaining",
-		"@babel/plugin-proposal-pipeline-operator",
+		
+		["@babel/plugin-proposal-optional-chaining", { "proposal": "smart" }],
+
+		["@babel/plugin-proposal-pipeline-operator", { "proposal": "smart" }],
 		
 		[
 			"@babel/plugin-transform-runtime",
